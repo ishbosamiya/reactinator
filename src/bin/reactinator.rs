58e2510901f90d5c5225f86ce56991d68ea617f4
ishbosamiya <ishbosamiya@gmail.com>
2023-11-13 22:59:50 +0530

    lib: basic setup

--- src/bin/reactinator.rs
+++ src/bin/reactinator.rs
@@ -1,5 +1,28 @@
 //! Reactinator binary.
 
-fn main() {
-    println!("Hello world!");
+use reactinator::Handler;
+use serenity::{model::prelude::*, Client};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let token = std::env::var("DISCORD_TOKEN")
+        .or_else(|_| std::fs::read_to_string("discord.token"))
+        .expect(
+            "Expected `DISCORD_TOKEN` environment variable \
+             or `./discord.token` file containing the token",
+        );
+
+    let mut client = Client::builder(
+        token,
+        GatewayIntents::non_privileged() | GatewayIntents::GUILD_MESSAGE_REACTIONS,
+    )
+    .event_handler(Handler)
+    .await
+    .expect("Couldn't create client");
+
+    if let Err(err) = client.start().await {
+        tracing::error!("client error: {}", err);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,51 @@
 //! Reactinator - Helper bot to react with any emoji.
+
+use serenity::{async_trait, model::prelude::*, prelude::*};
+
+/// Event handler.
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            tracing::info!("command interaction: {:#?}", command);
+
+            let content = match command.data.name.as_str() {
+                _ => Some("unimplemented_command".to_string()),
+            };
+
+            if let Some(content) = content {
+                if let Err(err) = command
+                    .create_interaction_response(&ctx.http, |response| {
+                        response
+                            .kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|message| message.content(content))
+                    })
+                    .await
+                {
+                    tracing::error!("couldn't respond to slash command due to `{}`", err);
+                }
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        tracing::info!("{} connected", ready.user.name);
+
+        for guild_id in ctx.cache.guilds().iter() {
+            let commands = guild_id
+                .set_application_commands(&ctx.http, |commands| commands)
+                .await;
+
+            match commands {
+                Ok(commands) => {
+                    tracing::info!("guild `{}` has the commands {:#?}", guild_id, commands);
+                }
+                Err(err) => {
+                    tracing::error!("couldn't create commands due to `{}`", err);
+                }
+            }
+        }
+    }
+}
