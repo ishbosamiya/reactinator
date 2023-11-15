981030d70cfca5e0a2c24bcfcd7eefd542aba842
ishbosamiya <ishbosamiya@gmail.com>
2023-11-15 17:26:44 +0530

    lib: delete bot reaction if user reacts

--- src/lib.rs
+++ src/lib.rs
@@ -210,4 +210,62 @@ impl EventHandler for Handler {
                     .map(|(_, emoji)| (emoji.name.clone(), emoji)),
             );
     }
+
+    async fn reaction_add(&self, context: Context, reaction: Reaction) {
+        if let (Some(guild_id), Some(user_id)) = (&reaction.guild_id, reaction.user_id) {
+            let guilds_to_bot_added_emojis = self.bot_context.bot_added_emojis.read().await;
+            if let Some(bot_added_emojis) = guilds_to_bot_added_emojis.get(guild_id) {
+                if let Some(delete_index) = bot_added_emojis.iter().enumerate().find_map(
+                    |(bot_added_emoji_index, bot_added_emoji)| {
+                        (bot_added_emoji.channel_id == reaction.channel_id
+                            && bot_added_emoji.message_id == reaction.message_id
+                            && bot_added_emoji.user_id == user_id)
+                            .then_some(bot_added_emoji_index)
+                    },
+                ) {
+                    drop(guilds_to_bot_added_emojis);
+                    let mut guilds_to_bot_added_emojis =
+                        self.bot_context.bot_added_emojis.write().await;
+                    let bot_added_emoji = guilds_to_bot_added_emojis
+                        .get_mut(guild_id)
+                        .unwrap()
+                        .swap_remove(delete_index);
+                    match reaction.message(&context.http).await {
+                        Ok(message) => {
+                            match message
+                                .delete_reaction(
+                                    &context.http,
+                                    Some(context.cache.current_user_id()),
+                                    bot_added_emoji.reaction_type,
+                                )
+                                .await
+                            {
+                                Ok(_) => {
+                                    tracing::info!(
+                                        "deleted reaction `{}` from `{}` since user reacted",
+                                        reaction.emoji,
+                                        reaction.message_id,
+                                    );
+                                }
+                                Err(err) => {
+                                    tracing::error!(
+                                        "unable to delete reaction `{:?}` due to `{}`",
+                                        reaction,
+                                        err
+                                    );
+                                }
+                            }
+                        }
+                        Err(err) => {
+                            tracing::error!(
+                                "couldn't get message for reaction `{:?}` due to `{}`",
+                                reaction,
+                                err
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
