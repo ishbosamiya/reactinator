600538ac162331848879636820d7442983001bea
ishbosamiya <ishbosamiya@gmail.com>
2023-11-16 10:32:07 +0530

    commands: text_to_reactions: create the command

--- src/commands/text_to_reactions.rs
+++ src/commands/text_to_reactions.rs
@@ -4,9 +4,228 @@
 use std::collections::HashMap;
 
 use lazy_static::lazy_static;
+use serenity::{
+    async_trait,
+    builder::CreateApplicationCommand,
+    json::Value,
+    model::{
+        application::interaction::InteractionResponseType,
+        prelude::{
+            application_command::ApplicationCommandInteraction, command::CommandOptionType,
+            MessageId, ReactionConversionError, ReactionType,
+        },
+    },
+};
+
+use crate::{context::BotAddedEmoji, BotContext};
+
+use super::Command;
+
+/// `text_to_reactions` command.
+pub struct TextToReactions;
+
+/// Option `text`.
+const OPTION_TEXT: &str = "text";
+
+/// Option `message_id`.
+const OPTION_MESSAGE_ID: &str = "message_id";
+
+#[async_trait]
+impl Command for TextToReactions {
+    fn register(command: &mut CreateApplicationCommand, _bot_context: &BotContext) -> Self {
+        command
+            .name("text_to_reactions")
+            .description(
+                "Text as list of reactions to the given \
+                 message or last message on the channel.",
+            )
+            .create_option(|command_option| {
+                command_option
+                    .required(true)
+                    .kind(CommandOptionType::String)
+                    .name(OPTION_TEXT)
+                    .description("Text to convert to reactions.")
+            })
+            .create_option(|command_option| {
+                command_option
+                    .kind(CommandOptionType::String)
+                    .name(OPTION_MESSAGE_ID)
+                    .description("Message ID to react to. Defaults to last message on channel.")
+            });
+        Self
+    }
+
+    async fn interaction(
+        &mut self,
+        command_interaction: &ApplicationCommandInteraction,
+        context: &serenity::prelude::Context,
+        bot_context: &BotContext,
+    ) {
+        let mut text_to_reactions_err = None;
+
+        let text = match command_interaction
+            .data
+            .options
+            .iter()
+            .find_map(|option| (option.name == OPTION_TEXT).then_some(option.value.as_ref()))
+        {
+            Some(Some(text)) => match text.as_str() {
+                Some(text) => Some(text),
+                None => {
+                    text_to_reactions_err = Some(Error::TextMustBeProvidedInString(text.clone()));
+                    None
+                }
+            },
+            _ => {
+                text_to_reactions_err = Some(Error::RequiresText);
+                None
+            }
+        };
+
+        let emoji_text = match text {
+            Some(text) => match text_to_emojis(&text) {
+                Some(emoji_text) => {
+                    tracing::info!("converted `{}` to `{}`", text, emoji_text);
+                    Some(emoji_text)
+                }
+                None => {
+                    text_to_reactions_err = Some(Error::CouldNotConvertTextToEmojis);
+                    None
+                }
+            },
+            None => None,
+        };
+
+        let message_id =
+            match command_interaction.data.options.iter().find_map(|option| {
+                (option.name == OPTION_MESSAGE_ID).then_some(option.value.as_ref())
+            }) {
+                Some(Some(message_id)) => match message_id.as_str() {
+                    Some(message_id) => match message_id.parse::<u64>().ok() {
+                        Some(message_id) => Some(MessageId(message_id)),
+                        None => {
+                            text_to_reactions_err =
+                                Some(Error::InvalidMessageId(message_id.to_string()));
+                            None
+                        }
+                    },
+                    None => {
+                        text_to_reactions_err =
+                            Some(Error::MessageIdMustBeString(message_id.clone()));
+                        None
+                    }
+                },
+                _ => None,
+            };
+
+        let message_id = match message_id {
+            Some(message_id) => Some(message_id),
+            None => bot_context
+                .last_message_ids
+                .read()
+                .await
+                .get(&command_interaction.channel_id)
+                .copied(),
+        };
+
+        if let Some(emoji_text) = &emoji_text {
+            match message_id {
+                Some(message_id) => {
+                    for emoji in emoji_text
+                        .split_whitespace()
+                        .map(|emoji| emoji.trim())
+                        .filter(|emoji| !emoji.is_empty())
+                    {
+                        match ReactionType::try_from(emoji) {
+                            Ok(reaction_type) => {
+                                match context
+                                    .http
+                                    .create_reaction(
+                                        command_interaction.channel_id.0,
+                                        message_id.0,
+                                        &reaction_type,
+                                    )
+                                    .await
+                                {
+                                    Ok(_) => {
+                                        tracing::info!(
+                                            "added reaction `{}` to `{}`",
+                                            reaction_type,
+                                            message_id
+                                        );
+
+                                        if let Some(guild_id) = command_interaction.guild_id {
+                                            bot_context
+                                                .bot_added_emojis
+                                                .write()
+                                                .await
+                                                .entry(guild_id)
+                                                .or_insert_with(Vec::new)
+                                                .push(BotAddedEmoji {
+                                                    channel_id: command_interaction.channel_id,
+                                                    message_id,
+                                                    user_id: command_interaction.user.id,
+                                                    reaction_type,
+                                                });
+                                        }
+                                    }
+                                    Err(err) => {
+                                        text_to_reactions_err =
+                                            Some(Error::CouldNotReactToMessage(err));
+                                    }
+                                }
+                            }
+                            Err(err) => {
+                                text_to_reactions_err = Some(Error::InvalidEmoji(err));
+                            }
+                        }
+                    }
+                }
+                None => {
+                    text_to_reactions_err =
+                        Some(Error::NoLastMessageAvailableAndNoMessageIdProvided);
+                }
+            }
+        }
+
+        if let Some(text_to_reactions_err) = &text_to_reactions_err {
+            tracing::error!(
+                target: "text_to_reactions",
+                "{}", text_to_reactions_err
+            );
+        }
+
+        if let Err(err) = command_interaction
+            .create_interaction_response(&context.http, |response| {
+                response
+                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                    .interaction_response_data(|message| {
+                        message
+                            .content(if let Some(err) = text_to_reactions_err {
+                                format!("error: {}", err)
+                            } else {
+                                format!(
+                                    "Don't forget to react to message `{}` \
+                                     yourself for the reactions {}.",
+                                    message_id.unwrap(),
+                                    emoji_text.unwrap(),
+                                )
+                            })
+                            .ephemeral(true)
+                    })
+            })
+            .await
+        {
+            tracing::error!(
+                "couldn't respond error message to slash command due to `{}`",
+                err
+            );
+        }
+    }
+}
 
 /// Text to emoji compatible text.
-pub fn text_to_emoji(text: &str) -> Option<String> {
+pub fn text_to_emojis(text: &str) -> Option<String> {
     lazy_static! {
         /// Alternatives for the [`char`]s.
         pub static ref ALTERNATIVES: HashMap<char, &'static [char]> = [
@@ -63,3 +282,40 @@ pub fn text_to_emoji(text: &str) -> Option<String> {
         })
         .collect()
 }
+
+/// `text_to_reactions` related errors.
+#[derive(Debug)]
+pub enum Error {
+    RequiresText,
+    TextMustBeProvidedInString(Value),
+    InvalidEmoji(ReactionConversionError),
+    MessageIdMustBeString(Value),
+    InvalidMessageId(String),
+    CouldNotReactToMessage(serenity::Error),
+    NoLastMessageAvailableAndNoMessageIdProvided,
+    CouldNotConvertTextToEmojis,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "text_to_reactions: ")?;
+        match self {
+            Error::RequiresText => write!(f, "requires text"),
+            Error::TextMustBeProvidedInString(value) => {
+                write!(f, "text must be provided in a string, got `{}`", value)
+            }
+            Error::InvalidEmoji(err) => write!(f, "invalid emoji: `{}`", err),
+            Error::MessageIdMustBeString(value) => {
+                write!(f, "message id must be a string, got `{}`", value)
+            }
+            Error::InvalidMessageId(value) => write!(f, "invalid message id, got `{}`", value),
+            Error::CouldNotReactToMessage(err) => write!(f, "could not react to message: {}", err),
+            Error::NoLastMessageAvailableAndNoMessageIdProvided => {
+                write!(f, "no last message available and no message id provided")
+            }
+            Error::CouldNotConvertTextToEmojis => write!(f, "could not convert text to emojis"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
