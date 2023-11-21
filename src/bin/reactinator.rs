7f4f0796582fe7f899e3843379031beb567fdbf6
ishbosamiya <ishbosamiya@gmail.com>
2023-11-21 13:14:50 +0530

    reactinator: support other paths to token

--- src/bin/reactinator.rs
+++ src/bin/reactinator.rs
@@ -1,18 +1,45 @@
 //! Reactinator binary.
 
+use std::path::PathBuf;
+
+use clap::Parser;
 use reactinator::Handler;
 use serenity::{model::prelude::*, Client};
 
+/// Reactinator
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct CommandLineArguments {
+    /// Discord token to use. Chosen over the path if present.
+    #[arg(env)]
+    pub discord_token: Option<String>,
+
+    /// Discord token provided through a path to file containing the
+    /// token.
+    #[arg(long, default_value("discord.token"))]
+    pub discord_token_path: PathBuf,
+}
+
 #[tokio::main]
 async fn main() {
+    let command_line_arguments = CommandLineArguments::parse();
     tracing_subscriber::fmt::init();
 
-    let token = std::env::var("DISCORD_TOKEN")
-        .or_else(|_| std::fs::read_to_string("discord.token"))
-        .expect(
-            "Expected `DISCORD_TOKEN` environment variable \
-             or `./discord.token` file containing the token",
-        );
+    tracing::info!(
+        target: "CommandLineArguments",
+        "{:?}",
+        command_line_arguments,
+    );
+
+    let token = command_line_arguments.discord_token.unwrap_or_else(|| {
+        std::fs::read_to_string(&command_line_arguments.discord_token_path).unwrap_or_else(|err| {
+            panic!(
+                "unable to read discord token at `{}` due to `{}`",
+                command_line_arguments.discord_token_path.display(),
+                err,
+            )
+        })
+    });
 
     let mut client = Client::builder(
         token,
